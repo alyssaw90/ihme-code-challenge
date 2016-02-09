0f2051bf052c78ac51b7926d83c89f2386ec39c0
Alyssa <alyssaw90@gmail.com>
2016-02-08 21:14:01 -0800

    setting up x and y axis

---
+++ script.js
@@ -0,0 +1,54 @@
+
+var HEIGHT = 600;
+var WIDTH = 1100;
+var MARGINS = {
+    top: 20,
+    right: 20,
+    bottom: 40,
+    left: 50
+  };
+
+//scales
+var x = d3.time.scale()
+          .range([0, (WIDTH - MARGINS.right - MARGINS.left)])
+          .domain([new Date(1990, 0, 1), new Date(2013, 0, 1)]);
+
+var y = d3.scale.linear()
+      .range([(HEIGHT - MARGINS.bottom - MARGINS.top), 0])
+      .domain([0, (0.75 * 100)]);
+
+var xAxis = d3.svg.axis()
+        .scale(x)
+        .ticks(10);
+  var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left');
+
+  var chart = d3.select('#chart')
+        .append('svg')
+        .attr('height', HEIGHT)
+        .attr('width', WIDTH)
+        .attr('id', 'display');
+
+  chart.append('g').call(xAxis)
+      .attr('transform', 'translate(' + MARGINS.left + ', ' +
+        (HEIGHT - MARGINS.bottom) + ')')
+      .attr('class', 'axis')
+      .selectAll('text')
+      .attr('y', 5)
+      .attr('x', 6)
+      .attr('dy', '.35em')
+      .attr('transform', 'rotate(45)')
+      .style('text-anchor', 'start');
+
+  chart.append('g').call(yAxis)
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + MARGINS.left + ', ' + MARGINS.top + ')')
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', 7)
+      .style('text-anchor', 'end')
+      .text('Mean Prevalence as Percentage of Population');
+
+
